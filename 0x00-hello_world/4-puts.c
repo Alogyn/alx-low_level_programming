311365fc3f0dc5faef663cd6aaac28acdef373bc
Alogyn <mohamed.derfoufi.dev@gmail.com>
2023-09-07 07:40:44 -0700

    Print exactly "Programming is like building a multilingual puzzle, followed by a new line.

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Begin/Start point
+ *
+ * Objectif: Print exactly "Programming is like building a multilingual puzzle
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
