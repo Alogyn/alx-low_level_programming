d57cd34e77c3d8903f1f72650542c4fa83992db7
Alogyn <mohamed.derfoufi.dev@gmail.com>
2023-09-08 14:00:43 -0700

    Print the lowercase alphabet in reverse, followed by a new line

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/*
+ * Task 7. Smile in the mirror
+ */
+
+/**
+  * main - Beginning/Starting point
+  *
+  * Objectif: Print the lowercase alphabet in reverse, followed by a new line
+  *
+  * Return: Always 0 (True)
+  */
+
+int main(void)
+{
+	/* Declaring the variable "letter" with the start value 'z' */
+	char letter = 'z';
+
+	/* Get and print alphabet from 'z' to 'a' */
+	while (letter >= 'a')
+	{
+		putchar(letter);
+		letter--;
+	}
+	putchar('\n');
+}
