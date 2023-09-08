5bc1513cee8aa04d17ab5b34c911982af33f0862
Alogyn <mohamed.derfoufi.dev@gmail.com>
2023-09-08 11:07:17 -0700

    Print the alphabet in lowercase, followed by a new line

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/*
+ * Task 2.I sometimes suffer from insomnia. And when I can't fall asleep,
+ * I play what I call the alphabet game
+ */
+
+/**
+  * main - Beginning/Starting point
+  *
+  * Objectif: Print the alphabet in lowercase, followed by a new line
+  *
+  * Return: Always 0 (True)
+  */
+
+int main(void)
+{
+	/* Declaring the variable "letter" with the start value 'a' */
+	char letter = 'a';
+
+	/* Get alphabet from 'a' to 'z' */
+	while (letter <= 'z')
+	{
+		putchar(letter);
+		letter++;
+	}
+	putchar("\n");
+	return (0);
+}
