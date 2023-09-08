59ed7f80bcfb1f38aa02da9e5d12f7c296a13c36
Alogyn <mohamed.derfoufi.dev@gmail.com>
2023-09-08 14:37:27 -0700

    Print all the numbers of base 16 in lowercase, followed by a new line

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/*
+ * Task 8. Hexadecimal
+ */
+
+/**
+  * main - Beginning/Starting point
+  *
+  * Objectif: Print all the numbers of base 16 in lowercase,
+  * followed by a new line
+  *
+  * Return: Always 0 (True)
+  */
+
+int main(void)
+{
+	/* Declaring the variable "hexa" with the value '0' */
+	char hexa = '0';
+
+	/* Get the number from 0 to 9 */
+	while (hexa <= '9')
+	{
+		putchar(hexa);
+		hexa++;
+	}
+
+	/* Get the alphabet from 'a' to 'f' */
+	while (hexa <= 'f')
+	{
+		putchar(hexa);
+		hexa++;
+	}
+	putchar('\n');
+	return (0);
+}
