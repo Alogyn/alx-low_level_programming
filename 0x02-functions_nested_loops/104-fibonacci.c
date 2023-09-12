e40d450543fad907dc8c3d258e2628b90de86c7c
Alogyn <mohamed.derfoufi.dev@gmail.com>
2023-09-12 15:25:13 -0700

    Find and print the first 98 Fibonacci numbers, starting with 1 and 2, followed by a new line

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,6 +7,32 @@
  * My teacher gave me an A+
  */
 
+/*
+ * length - Get the length
+ *
+ * @num: Input number or argument
+ *
+ * Objectif: Handles unsigned long numbers
+ *
+ * return: 'a', number of digit
+ */
+
+int length(unsigned long num)
+{
+	int a = 0;
+
+	if (!num)
+	{
+		return (1);
+	}
+	while (num)
+	{
+		num = num / 10;
+		a += 1;
+	}
+	return (a);
+}
+
 /**
   * main - Beginning/Starting point
   *
@@ -18,17 +44,43 @@
 
 int main(void)
 {
-	int i, n = 98;
-	unsigned  int  fib1 = 1, fib2 = 2, next;
+	int i, j;
+	unsigned long int fib1 = 1, fib2 = 2, sum;
+	unsigned long int f1o = 0, f2o = 0, sumo = 0;
+	unsigned long int mx = 1000000000000000000UL;
 
-	printf("%u, %u", fib1, fib2);
-	for (i = 2; i < n; i++)
+	for (i = 1, i <= 98; i++)
 	{
-		next = fib1 + fib2;
-		printf(", %u", next);
-		 fib1 = fib2;
-		 fib2 = next;
+		if (f1o > 0)
+		{
+			printf("%lu", f1o);
+		}
+		j = length(mx) - 1 - length(f1);
+		while (f1o > 0 && j > 0)
+		{
+			 printf("%d", 0);
+			 --j;
+		}
+		printf("%lu", f1);
+		sum = f1 + f2;
+		if (sum >= mx)
+		{
+			sum -= mx;
+			sumo += 1;
+		}
+		f1 = f2;
+		f1o = f2o;
+		f2 = sum;
+		f2o = sumo;
+
+		if (i != 98)
+		{
+			printf(", ");
+		}
+		else
+		{
+			printf("\n");
+		}
 	}
-	printf("\n");
 	return (0);
 }
