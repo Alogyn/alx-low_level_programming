468cfbf204813aab83f9057fd1897488e1dee587
Alogyn <mohamed.derfoufi.dev@gmail.com>
2023-09-12 05:41:18 -0700

    Print _putchar, followed by a new line

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/*
+ * Task 0. _putchar
+ */
+
+/**
+  * main - Beginning/Starting point
+  *
+  * Objectif: Print _putchar, followed by a new line
+  *
+  * Return: Always 0 (True)
+  */
+
+int main(void)
+{
+	printf("\_putchar\n");
+	return (0);
+}
