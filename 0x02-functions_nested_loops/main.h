77199e95beefed538832e0374175224be39f6341
Alogyn <mohamed.derfoufi.dev@gmail.com>
2023-09-12 09:45:30 -0700

    Compute the absolute value of an integer

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,11 +1,19 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/* Task 0 */
 int _putchar(char c);
+/* Task 1 */
 void print_alphabet(void);
+/* Task 2 */
 void print_alphabet_x10(void);
+/* Task 3 */
 int _islower(int c);
+/* Task 4 */
 int _isalpha(int c);
+/* Task 5 */
 int print_sign(int n);
+/* Task 6 */
+int _abs(int n);
 
 #endif
