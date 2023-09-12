b8d1bf0248f96847fb8e0595e7f269514baff4de
Alogyn <mohamed.derfoufi.dev@gmail.com>
2023-09-12 13:26:57 -0700

    Print the n times table, starting with 0

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -26,10 +26,7 @@ void print_times_table(int n)
 			for (j = 0; j <= n; j++)
 			{
 				result = i * j;
-				if (result == 0)
-				{
-					_putchar(result + '0');
-				} else if (result < 10 && result != 0)
+				if (result < 10)
 				{
 					_putchar(',');
 					_putchar(' ');
