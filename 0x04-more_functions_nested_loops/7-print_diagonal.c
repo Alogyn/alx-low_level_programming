68b179235603843f0ffa81b9563ef36d36df2d40
Alogyn <mohamed.derfoufi.dev@gmail.com>
2023-09-14 12:25:45 -0700

    Draw a diagonal line on the terminal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/*
+ * Task 7. I feel like I am diagonally parked in a parallel universe
+ */
+
+/**
+  * print_diagonal - Beginning/Starting point
+  *
+  * @n: Input number or argument
+  *
+  * Objectif: Draw a diagonal line on the terminal
+  *
+  * Return: None
+  */
+
+void print_diagonal(int n)
+{
+	/* Varaibles declaration */
+	int i, j;
+
+	if (n > 0)
+	{
+		/* '\' lopp */
+		for (i = 0; i < n; i++)
+		{
+			/* Space lopp */
+			for (j = 0; j < i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar(92);
+		}
+	}
+	else
+	{
+		_putchar('_');
+	}
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -16,5 +16,7 @@ void print_most_numbers(void);
 void more_numbers(void);
 /* Task 6 */
 void print_line(int n);
+/* Task 7 */
+void print_diagonal(int n);
 
 #endif
