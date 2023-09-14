5a6ff5ae01638c6300672052bf9982cba0a915ef
Alogyn <mohamed.derfoufi.dev@gmail.com>
2023-09-14 08:38:09 -0700

    Check for uppercase character

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,27 @@
+#include "main"
+
+/*
+ * Task 0. isupper
+*/
+
+/**
+  * _isupper - Beginning/Starting point
+  *
+  * Objectif: Check for uppercase character
+  *
+  * Return: 1 if character is 'Uppercase', else 0
+  *
+  */
+
+int _isupper(int c)
+{
+	/* 'A' == 65 and 'Z == 90 in ASXII table */
+	if (c > 64 && c < 91)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,7 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Task 0 */
+int _isupper(int c);
+
+#endif
