06746f42f567fcffe54e373dff4ec5bc89cae11b
Alogyn <mohamed.derfoufi.dev@gmail.com>
2023-09-14 14:12:19 -0700

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -22,17 +22,17 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0)
+		if ((i % 3 == 0) && (i % 5 == 0))
+		{
+			printf("FizzBuzz");
+		}
+		else if (i % 3 == 0)
 		{
 			printf("Fizz");
 		}
 		else if (i % 5 == 0)
 		{
 			printf("Buzz");
-		}
-		else if ((i % 3 == 0) && (i % 5 == 0))
-		{
-			printf("FizzBuzz");
 		}
 		else
 		{
