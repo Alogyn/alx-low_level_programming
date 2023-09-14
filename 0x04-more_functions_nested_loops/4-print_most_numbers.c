733619302ba96c4f08d81904c7f2ccaac2b9044f
Alogyn <mohamed.derfoufi.dev@gmail.com>
2023-09-14 09:37:55 -0700

    Print the numbers, from 0 to 9, followed by a new line

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/*
+ * Task 4. I believe in numbers and signs
+ */
+
+/**
+  * print_most_numbers - Beginning/Starting point
+  *
+  * Objectif: Print the numbers, from 0 to 9, followed by a new line
+  *
+  * Return: None
+  */
+
+void print_most_numbers(void)
+{
+	/* Variable declaration */
+	int n = 0;
+
+	while (n < 10)
+	{
+		if (n != 2 || n != 4)
+		{
+			_putchar(n + '0');
+		}
+		n++;
+	}
+	_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -5,10 +5,12 @@
 int _isupper(int c);
 /* Task 1 */
 int _isdigit(int c);
-/* Task 3 */
+/* Task 2 */
 int mul(int a, int b);
-/* Task 4 */
+/* Task 3 */
 void print_numbers(void);
 int _putchar(char c);
+/* Task 4 */
+void print_most_numbers(void);
 
 #endif
