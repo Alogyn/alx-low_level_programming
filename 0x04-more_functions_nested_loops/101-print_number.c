5fc5c52b74d282343902edfc21471e70cb470664
Alogyn <mohamed.derfoufi.dev@gmail.com>
2023-09-14 15:55:04 -0700

    Print an integer

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -17,17 +17,20 @@
 
 void print_number(int n)
 {
+	/* Variable declaration */
+	unsigned int m;
+
 	/* When the number is negative */
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		m = -n;
 	}
 	/* When the number is positive */
-	if (n / 10)
+	if (m / 10)
 	{
-		print_number(n / 10);
+		print_number(m / 10);
 	}
-	_putchar((n % 10) + '0');
+	_putchar((m % 10) + '0');
 	_putchar ('\n');
 }
