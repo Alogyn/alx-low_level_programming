39ae1e93ce22fead64c869832da5ad48ee1814d9
Alogyn <mohamed.derfoufi.dev@gmail.com>
2023-09-18 05:53:59 -0700

    Swap the values of two integers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/*
+ * Task 1. Don't swap horses in crossing a stream
+ */
+
+/**
+  * swap_int - Beginning/Starting point
+  *
+  * @a: Fiorst pointer or argument
+  * @b: Second pointer or argument
+  *
+  * Objectif: Swap the values of two integers
+  *
+  * Return: None
+  */
+
+void swap_int(int *a, int *b)
+{
+	/* Declaration of memory variable */
+	int memory;
+
+	/* Swap the values of 'a' and 'b' */
+	c = *a;
+	*b = *a;
+	*a = c;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,5 +3,7 @@
 
 /* Task 0 */
 void reset_to_98(int *n);
+/* Task 1 */
+void swap_int(int *a, int *b);
 
 #endif
