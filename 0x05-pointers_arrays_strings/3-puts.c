0bfce103f3533a9ee337c8615b0e335109f74b62
Alogyn <mohamed.derfoufi.dev@gmail.com>
2023-09-18 06:47:52 -0700

    Print a string, followed by a new line, to stdout

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -17,9 +17,10 @@
 void _puts(char *str)
 {
 	/* Loop to Print every character from the string */
-	while (*str != '\0'; *str++)
+	while (*str != '\0')
 	{
 		_putchar(*str + '0');
+		*str++;
 	}
 	_putchar('\n');
 }
