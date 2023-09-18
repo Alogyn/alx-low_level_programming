b4fa7a97f87fafa2461a0bf13976f0561802921a
Alogyn <mohamed.derfoufi.dev@gmail.com>
2023-09-18 05:42:48 -0700

    Take a pointer to an int as parameter and updates the value it points to to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/*
+ * Task 0. 98 Battery st.
+ */
+
+/**
+  * reset_to_98 - Beginning/Starting point
+  *
+  * @n: pointer or argument
+  *
+  * Objectif: Take a pointer to an int as parameter and updates the value
+  * it points to to 98
+  *
+  * Return: None
+  */
+
+void reset_to_98(int *n)
+{
+	/* Give the pointer address 'n' the value '98' */
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,7 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Task 0 */
+void reset_to_98(int *n);
+
+#endif
