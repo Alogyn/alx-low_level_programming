f72c3becec762976bc2178c903058e2bfb5055fb
Alogyn <mohamed.derfoufi.dev@gmail.com>
2023-09-18 06:44:27 -0700

    Print a string, followed by a new line, to stdout

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/*
+ * Task 3. I do not fear computers. I fear the lack of them
+ */
+
+/**
+  * _puts - Beginning/Starting point
+  *
+  * @str: Pointer or argument
+  *
+  * Objectif: Print a string, followed by a new line, to stdout
+  *
+  * Return: None
+  */
+
+void _puts(char *str)
+{
+	/* Loop to Print every character from the string */
+	for (*str != '\0'; *str++)
+	{
+		_putchar(*str + '0');
+	}
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -7,5 +7,8 @@ void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 /* Task 2 */
 int _strlen(char *s);
+/* Task 3 */
+int _putchar(char c);
+void _puts(char *str);
 
 #endif
