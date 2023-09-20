2941d9d59aded2bd0a750121ba57d5aa22625de5
Alogyn <mohamed.derfoufi.dev@gmail.com>
2023-09-20 07:09:45 -0700

    Concatenate two strings v2

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/*
+ * Task 1. strncat
+ */
+
+/**
+  * _strncat - Beginning/Starting point
+  *
+  * @src: Original string pointer or argument
+  * @dest: Append.v2 string pointer or argument
+  * @n: Byts number used from the string 'src'
+  *
+  * Objectif: Concatenate two strings
+  *
+  * Return: The pointer to dest
+  */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	/* Append variable declaration */
+	int append = 0;
+	/* String size count variable declaration */
+	int count = 0;
+
+	/* String size count loop */
+	while (dest[count] != 0)
+	{
+		count++;
+	}
+
+	/* Concatenates two strings */
+	for (; append < n && src[append] != 0; append++)
+	{
+		dest[append + count] = src[append];
+	}
+
+	/* Terminating null byte */
+	dest[append + count] = '\0';
+
+	return (dest);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -3,5 +3,7 @@
 
 /* Task 0 */
 char *_strcat(char *dest, char *src);
+/* Task 1 */
+char *_strncat(char *dest, char *src, int n);
 
 #endif
