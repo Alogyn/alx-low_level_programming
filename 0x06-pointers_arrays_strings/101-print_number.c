dacf8e855697af891f4791429c2b6cad123827c7
Alogyn <mohamed.derfoufi.dev@gmail.com>
2023-09-21 08:07:23 -0700

    Print an integer

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -26,44 +26,25 @@ void print_number(int n)
 		return;
 	}
 
-	/* Negative number */
-	else if (n < 0 && n > -9)
+	/* Negative case */
+	if (n < 0)
 	{
-		_putchar(n);
-		return;
-	}
-
-	/* Positive number */
-	else if (n > 0 && n < 9)
-	{
-		_putchar(n);
-		return;
+		_putchar('-');
+		n = -n;
 	}
 
 	/* Digit checker loop */
-	while (n != 0 && (n > 9 || n < -9))
+	while (n > 0)
 	{
-		/* negative case */
-		if (n < -9)
-		{
-			_putchar('-');
-			n *= -1;
-			n /= 10;
-			digit *= 10;
-		}
-
-		/* Positive case */
-		else if (n > 9)
-		{
-			n /= 10;
-			digit *= 10;
-		}
+		n /= 10;
+		digit *= 10;
 	}
 
 	/* Print the digits */
-	while (digit > 0)
+	while (digit > 1)
 	{
-		_putchar(((n / digit) % 10) + '0');
 		digit /= 10;
+		_putchar(((n / digit) % 10) + '0');
 	}
+}
 }
