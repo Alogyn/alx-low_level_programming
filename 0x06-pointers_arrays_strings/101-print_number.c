bafd0812e6b6d4afbc5c551c4743e9fb1ed5791a
Alogyn <mohamed.derfoufi.dev@gmail.com>
2023-09-21 10:02:41 -0700

    Print an integer

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -16,17 +16,21 @@
 
 void print_number(int n)
 {
+	/* Long numbers variable declaration */
+	unsigned int number;
+
+	number = n;
 	/* Negative number */
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		number = -n;
 	}
 
 	/* Print all numbers */
-	if (n / 10 != 0)
+	if (number / 10 != 0)
 	{
-		print_number(n / 10);
+		print_number(number / 10);
 	}
-	_putchar((n % 10) + '0');
+	_putchar((number % 10) + '0');
 }
