05ddc3868030f630b0c415abbced32896d4d74f9
Alogyn <mohamed.derfoufi.dev@gmail.com>
2023-09-25 08:47:23 -0700

    Get the length of a prefix substring

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/*
+ * Task 3. strspn
+ */
+
+/**
+  * _strspn - Get the length of a prefix substring
+  *
+  * @s: Input pointer to string (char)
+  * @accept: Number of Bytes accepted
+  *
+  * Return: The number of bytes in the initial segment of 's'
+  * which consist only of bytes from 'accept'
+  */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	/* Loops and breaker variables declaration */
+	unsigned int length = 0, checker;
+	bool breaker;
+
+	/* Length counter loop */
+	while (*s != '\0')
+	{
+		/* Initializing variables */
+		checker = 0;
+		breaker = false;
+		/* Checker 'accept' loop */
+		while (accept[checker] != '\0')
+		{
+			/* Finding consist char */
+			if (*s ==  accept[checker])
+			{
+				breaker = true;
+				break;
+			}
+			checker++;
+		}
+
+		/* in case not consist char */
+		if (breaker == false)
+		{
+			break;
+		}
+		s++;
+	}
+
+	return (s);
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -7,5 +7,7 @@ char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 /* Task 2 */
 char *_strchr(char *s, char c);
+/* Task 3 */
+unsigned int _strspn(char *s, char *accept);
 
 #endif
