0e0134f9014a459897da707ff770e7d4d5b15895
Alogyn <mohamed.derfoufi.dev@gmail.com>
2023-09-28 06:36:18 -0700

    Print a string, followed by a new line

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,12 +8,23 @@
 /**
   * _puts_recursion - Prints a string, followed by a new line
   *
-  * @s:
+  * @s: Input string pointer (char)
   *
   * Return: None
   */
 
 void _puts_recursion(char *s)
 {
-	/* */
+	/* Print the characters from the string */
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
 
+	/* Retrun to line if no characters left */
+	else
+	{
+		_putchar('\n');
+	}
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+int _putchar(char c);
 /* Task 0 */
 void _puts_recursion(char *s);
 
