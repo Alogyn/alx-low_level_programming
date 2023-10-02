1eb82f7d795a4b9751ee7afac76554aa808d7429
Alogyn <mohamed.derfoufi.dev@gmail.com>
2023-10-02 02:09:10 -0700

    Print all arguments it receives

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/*
+ * Task 2. The best argument against democracy is a five-minute conversation
+ * with the average voter
+ */
+
+/**
+  * main - Prints all arguments it receives
+  *
+  * @argc: Number of arguments
+  * @argv: Array arguments pointer
+  *
+  * Return: Always 0 (Success)
+  */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	/* Loop variable declaration */
+	int i = 0;
+
+	/* Print all arguments */
+	while (i < argc)
+	{
+		printf("%s\n", argv[i]);
+		i++;
+	}
+	return (0);
+}	
