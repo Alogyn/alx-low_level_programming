26c1d6485bb81f9ff8e7dae12d4967e282ccfcb6
Alogyn <mohamed.derfoufi.dev@gmail.com>
2023-10-04 15:48:03 -0700

    Return a pointer to a 2 dimensional array of integers

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,59 @@
+#include "main.h"
+#include <stdlib.h>
+
+/*
+ * Task 3. If you even dream of beating me you'd better wake up and apologize
+ */
+
+/**
+  * alloc_grid - Returns a pointer to a 2 dimensional array of integers
+  *
+  * @width: Input dimension width (int)
+  * @height: Input dimension height (int)
+  *
+  * Return: The pointer of 2 dimensional array of integers,
+  * Null in case nothing inputed
+  */
+
+int **alloc_grid(int width, int height)
+{
+	/* Pointer and loop variables declarations */
+	int **grid;
+	int i = 0, j = 0;
+
+	/* In case no input */
+	if (width < 1 || height < 1)
+		return (NULL);
+	/* Memory for the dimesion height */
+	grid = malloc(height * sizeof(int *));
+	if (grid == NULL)
+		return (NULL);
+	while (i < height)
+	{
+		/* Memory for the dimension width */
+		grid[i] = malloc(width * sizeof(int *));
+		/* In case fail */
+		if (*(grid + i) == NULL)
+		{
+			while (i >= 0)
+			{
+				free(grid[i]);
+				i--;
+			}
+			free(grid);
+			return (NULL);
+		}
+		i++;
+	}
+		/* In case success */
+		while (i < height)
+		{
+			while (j < width)
+			{
+				grid[i][j] = 0;
+				j++;
+			}
+			i++;
+		}
+	return (grid);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -7,5 +7,7 @@ char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 /* Task 2 */
 char *str_concat(char *s1, char *s2);
+/* Task 3 */
+int **alloc_grid(int width, int height);
 
 #endif
