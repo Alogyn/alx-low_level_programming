16303c1d1b0dfd24c6a9c42421ea0894477276bf
Alogyn <mohamed.derfoufi.dev@gmail.com>
2023-10-08 11:19:07 -0700

    Prototypes functions

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -11,5 +11,6 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 /* Task 4 */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+x
 
 #endif
