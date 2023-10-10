48397dc963351da305409d4ba3db6d5d45c4d3d2
Alogyn <mohamed.derfoufi.dev@gmail.com>
2023-10-10 02:27:43 -0700

    Compute the sum of the numbers x and y

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,14 @@
+/*
+ * Task 4. SUM
+ */
+
+/**
+  * Objectif: Computes the sum of the numbers 'x' and 'y'
+  */
+
+#ifndef SUM_H
+#define SUM_H
+
+#define SUM(x, y) ((x) + (y))
+
+#endif /* SUM_H */
