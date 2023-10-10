0e0652ebd9d379c9e040cc8677662ad23effbf6c
Alogyn <mohamed.derfoufi.dev@gmail.com>
2023-10-10 03:16:53 -0700

    Define a macro named SIZE as an abbreviation for the token 1024

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -6,8 +6,8 @@
   * Objectif: Defines a macro named SIZE as an abbreviation for the token 1024
 */
 
-#ifndef 0_OBJECT_LIKE_MACRO_H
-#define 0_OBJECT_LIKE_MACRO_H
+#ifndef _0_OBJECT_LIKE_MACRO_H
+#define _0_OBJECT_LIKE_MACRO_H
 
 #define SIZE 1024
 
