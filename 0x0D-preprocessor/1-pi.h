45d3dc2c3e9bf6cfe2d20b11e72961bd80b4d44b
Alogyn <mohamed.derfoufi.dev@gmail.com>
2023-10-10 01:34:24 -0700

    Define a macro named PI as an abbreviation for the token 3.14159265359

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,14 @@
+/*
+ * Task 1. Pi
+ */
+
+/**
+  * Objectif: Defines a macro named PI as an abbreviation for the token 3.14159265359
+  */
+
+#ifndef PI_H
+#define PI_H
+
+#define PI 3.14159265359 
+
+#endif /* PI_H */
