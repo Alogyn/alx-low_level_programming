b3b2c9222e5ff72c8e9e6b230ecbbe34d7faec9d
Alogyn <mohamed.derfoufi.dev@gmail.com>
2023-10-10 02:08:31 -0700

    the absolute value of a number x

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,14 @@
+/*
+ * Task 3. Function-like macro
+ */
+
+/**
+  * Objectif: Computes the absolute value of a number 'x'
+  */
+
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+#define ABS(x) ((x) > (0) ? (x) : -(x))
+
+#endif /* FUNCTION_LIKE_MACRO_H */
