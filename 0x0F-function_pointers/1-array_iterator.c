44661304fe036eb65d137cd00cd13b6ca70333a7
Alogyn <mohamed.derfoufi.dev@gmail.com>
2023-10-11 04:12:14 -0700

    Execute a function given as a parameter on each element of an array

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,35 @@
+#include "function_pointers.h"
+
+/*
+ * Task 1. If you spend too much time thinking about a thing,
+ * you'll never get it done
+ */
+
+/**
+  * array_iterator - Executes a function given as a parameter
+  * on each element of an array
+  *
+  * @array : Input an array (int)
+  * @size: Input size of ana array (structure size_t)
+  * @action: Pointer to the function (void)
+  *
+  * Return: None
+  */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	/* Loop variable declaration */
+	unsigned int i = 0;
+
+	/* Checks if the 'array' and the 'size' are not empty */
+	if (array != NULL && action != NULL)
+	{
+		/* Loop to run the function 'action' */
+		while (i < size)
+		{
+			/* The function pointer */
+			action(array[i]);
+			i++;
+		}
+	}
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,12 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 
+/* Libreries */
+#include <stddef.h>
+
 /* Task 0 */
 void print_name(char *name, void (*f)(char *));
+/* Task 1 */
+void array_iterator(int *array, size_t size, void (*action)(int));
 
 #endif /* FUNCTION_POINTERS_H */
