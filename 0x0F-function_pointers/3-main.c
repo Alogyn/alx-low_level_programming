42f1b83aac36dc1e702687a61931b7ec7d0bd9c1
Alogyn <mohamed.derfoufi.dev@gmail.com>
2023-10-11 06:55:40 -0700

    main.c files

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,14 +1,37 @@
 #include "3-calc.h"
 
 /**
-  * main - 
+  * main - Beginning/Starting point
   *
   * @argc: Number of arguments (int)
-  * @argv: Array arguments pointer (char)
+  * @argv: Array arguments (char)
   *
-  * Return: Always 0 (True)
+  * Return: '0' if success, '98' if argument number is wrong,
+  * 99 if invalid operator
   */
 
 int main(int argc, char *argv[])
 {
+	/* Appropriate function based on the operator */
+	int (*operation)(int, int);
+
+	/* Checks the number of arguments */
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	/* Checks the invalid operator */
+	operation = get_op_func(argv[2]);
+
+	if (!operation)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	printf("%d\n", operation(atoi(argv[1]), atoi(argv[3])));
+
+	return (0);
 }
