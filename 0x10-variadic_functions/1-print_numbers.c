a99c3c54d113c4c56c071b9378c469cdf0a5b5fc
Alogyn <mohamed.derfoufi.dev@gmail.com>
2023-10-12 04:35:08 -0700

    Print numbers, followed by a new line

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,47 @@
+#include "variadic_functions.h"
+
+/*
+ * Task 1. To be is to be the value of a variable
+ */
+
+/**
+  * print_numbers - Prints numbers, followed by a new line
+  *
+  * @separator: The string to seprate two integers (char)
+  * @n: The number of arguments (int)
+  *
+  * Return: None
+  */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list print_num;
+	unsigned int i = 0;
+
+	va_start(print_num, n);
+
+	if (separator == NULL)
+	{
+		separator = "";
+	}
+
+	else
+	{
+		while (i < n)
+		{
+			if (i == n - 1)
+			{
+				printf("%d\n", va_arg(print_num, int));
+			}
+
+			else
+			{
+				printf("%d", va_arg(print_num, int));
+				printf("%s", separator);
+			}
+			i++;
+		}
+	}
+
+	va_end(print_num);
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -3,8 +3,11 @@
 
 /* Libreries */
 #include <stdarg.h>
+#include <stdio.h>
 
 /* Task 0 */
 int sum_them_all(const unsigned int n, ...);
+/* Task 1 */
+void print_numbers(const char *separator, const unsigned int n, ...);
 
 #endif /* VARIADIC_FUNCTIONS_H */
