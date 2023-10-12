f50045ba43cd64ff770eb84e209238c63fcdbe12
Alogyn <mohamed.derfoufi.dev@gmail.com>
2023-10-12 12:29:43 -0700

    Prototypes functions and libreries

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -11,5 +11,7 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 /* Task 2 */
 void print_strings(const char *separator, const unsigned int n, ...);
+/* Task 3 */
+void print_all(const char * const format, ...);
 
 #endif /* VARIADIC_FUNCTIONS_H */
