4691080eb8b5c6223c44c0b8a6937e523ba7099f
Alogyn <mohamed.derfoufi.dev@gmail.com>
2023-10-12 12:18:38 -0700

    Print strings, followed by a new line

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -5,10 +5,51 @@
  */
 
 /**
+  * print_strings - Prints strings, followed by a new line
   *
+  * @separator: The string to seprate two integers (char)
+  * @n: The number of arguments (int)
+  *
+  * Returnn: None
   */
 
 void print_strings(const char *separator, const unsigned int n, ...)
 {
+	/* Variadic, pointer and loop variable declarations */
+	va_list print_str;
+	unsigned int i = 0;
+	char *str;
 
+	/* Beginning of the 'print_str' variadic */
+	va_start(print_str, n);
+
+	/* In case 'separator' is empty */
+	if (separator == NULL)
+	{
+		separator = "";
+	}
+
+	/* In case 'n' valid, checks and prints the characters */
+	while (i < n)
+	{
+		str = va_arg(print_str, char*);
+		if (str == NULL)
+		{
+			str = "(nil)";
+		}
+		else if (i < n - 1)
+		{
+			printf("%s", str);
+		}
+		else
+		{
+			printf("%s", separator);
+		}
+		i++;
+	}
+
+	printf("\n");
+
+	/* Ending of the 'print_str' variadic */
+	va_end(print_str);
 }
