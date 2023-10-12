3b1477c05b33df1865498e2719f14c5725ecb760
Alogyn <mohamed.derfoufi.dev@gmail.com>
2023-10-12 12:21:40 -0700

    Print strings, followed by a new line

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -40,10 +40,11 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		else if (i < n - 1)
 		{
 			printf("%s", str);
+			printf("%s", separator);
 		}
 		else
 		{
-			printf("%s", separator);
+			printf("%s", str);
 		}
 		i++;
 	}
