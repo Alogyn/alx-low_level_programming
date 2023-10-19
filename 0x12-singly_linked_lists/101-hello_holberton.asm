4ba9b30cee246b047bdd1c78f0eb225b1f1308a2
Alogyn <mohamed.derfoufi.dev@gmail.com>
2023-10-19 15:06:47 -0700

    Write a 64-bit program in assembly that prints Hello, Holberton, followed by a new line

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,14 +1,16 @@
-section .data
-    hello db "Hello, Holberton", 10, 0  ; 10 is the ASCII code for newline, 0 is the null terminator
+SECTION .data
+    msg db "Hello, Holberton", 0    ; Null-terminated string
+    fmt db "%s", 10, 0              ; Format string: "%s\n"
 
-section .text
+SECTION .text
+    extern printf
     global main
 
-extern printf
-
 main:
-    lea rdi, [hello]  ; Load the address of the hello string into rdi register
+    mov rdi, fmt      ; Load the format string address into rdi register
+    mov rsi, msg      ; Load the message address into rsi register
+    xor rax, rax      ; Clear RAX register (used for the printf return value)
     call printf       ; Call printf function to print the string
-    mov rax, 60       ; syscall: exit
-    xor rdi, rdi      ; status: 0
-    syscall           ; invoke syscall to exit the program
+
+    xor rax, rax      ; Set return value to 0 (exit status)
+    ret               ; Return from the main function
