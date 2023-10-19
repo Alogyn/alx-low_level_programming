5af90f56f3d66fad98d5b4756b954527442423f2
Alogyn <mohamed.derfoufi.dev@gmail.com>
2023-10-19 13:33:58 -0700

    Print all the elements of a list_t list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/* Task 0. Print list */
+
+/**
+  * print_lis - Prints all the elements of a 'list_t' list
+  *
+  * @h: List name
+  *
+  * Return: The number of nodes
+  */
+
+size_t print_list(const list_t *h)
+{
+	/* Loop counter variable declaration */
+	int i = 0;
+
+	while (h != 0)
+	{
+		/* Check the current node */
+		if (h->str != 0)
+			printf("[%d] %s\n", h->len, h->str);
+		else
+			printf("[0] (nil)\n");
+		i++;
+		h = h->next;
+	}
+
+	/* Return the number of counter node */
+	return (i);
+}
