02b31ee2c2e36b98682a5085552116c5a9102464
Alogyn <mohamed.derfoufi.dev@gmail.com>
2023-10-24 12:00:53 -0700

    Insert a new node at a given position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -40,7 +40,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	/* Find the node at position idx - 1 */
 	current = *head;
-	for (i = 0; current; i++)
+	for (i = 0; i < idx - 1 && current; i++)
 		current = current->next;
 
 	/* in case idx out of bound */
