7c8b6e081946865c1d48b79f12f09feb6c0e6c96
Alogyn <mohamed.derfoufi.dev@gmail.com>
2023-10-24 14:48:48 -0700

    Prototypes and structure functions

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -48,5 +48,7 @@ listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 /* Task 13 */
 size_t free_listint_safe(listint_t **h);
+/* Task 14 */
+listint_t *find_listint_loop(listint_t *head);
 
 #endif /* LISTS_H */
