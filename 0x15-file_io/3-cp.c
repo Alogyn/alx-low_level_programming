e56ac9123c22c5f1836b500d6baea2ebcbe279fe
Alogyn <mohamed.derfoufi.dev@gmail.com>
2023-10-30 16:49:19 -0700

    incitatous file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -6,6 +6,10 @@
 
 #define BUFF_SIZE 1024
 
+#define USAGE_ERROR 97
+#define READ_ERROR 98
+#define WRITE_ERROR 99
+
 /**
   * print_error_and_exit - Prints an error message and exits the program
   *
@@ -17,7 +21,7 @@
 
 void print_error_and_exit(const char *error_msg, int exit_code)
 {
-	dprintf(2, "%s\n", error_msg);
+	dprintf(STDERR_FILENO, "%s\n", error_msg);
 	exit(exit_code);
 }
 
@@ -53,7 +57,7 @@ int open_file(const char *filename, int flags, mode_t mode, int exit_code)
 
 	if (fd == -1)
 	{
-		dprintf(2, "Error: Can't open file %s\n", filename);
+		dprintf(STDERR_FILENO, "Error: Can't open file %s\n", filename);
 		exit(exit_code);
 	}
 
@@ -72,28 +76,33 @@ int open_file(const char *filename, int flags, mode_t mode, int exit_code)
 void copy_file(const char *file_from, const char *file_to)
 {
 	/* Open source and destination files */
-	int source_fd = open_file(file_from, O_RDONLY, 0, 98);
+	int source_fd = open_file(file_from, O_RDONLY, 0, READ_ERROR);
 	int destination_fd = open_file(file_to, O_WRONLY | O_CREAT | O_TRUNC,
-			S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH, 99);
+			S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | 
+			_IROTH, WRITE_ERROR);
 	char buffer[BUFF_SIZE];
-	ssize_t file_read, file_write;
+	ssize_t file_read, file_write, total_write = 0;
 
 	/* Copy content from source to destination in chunks */
-	while ((file_read = read(source_fd, buffer, BUFF_SIZE)) > 0)
+	while ((file_read = read(source_fd, buffer + total_read,
+					BUFF_SIZE - total_read)) > 0)
 	{
-		file_write = write(destination_fd, buffer, file_read);
-		/* Handle write errors */
-		if (file_write != file_read || file_write == -1)
-			print_error_and_exit("Error: Can't write to file", 99);
+		total_read += file_read;
+		if (total_read == BUFF_SIZE)
+			file_write = write(destination_fd, buffer, BUFF_SIZE);
+		else
+			file_write = write(destination_fd, buffer, total_read);
+		if (file_write <= 0)
+			print_error_and_exit("Error: Can't write to file",
+					WRITE_ERROR);
+		if (total_read == BUFF_SIZE)
+			total_read = 0;
 	}
-
-	/* Handle read errors */
 	if (file_read == -1)
-		print_error_and_exit("Error: Can't read from file", 98);
+		print_error_and_exit("Error: Can't read from file", READ_ERROR);
 
-	/* Close file descriptors */
-	close_file_descriptor(source_fd, 98);
-		close_file_descriptor(destination_fd, 99);
+	close_file_descriptor(source_fd, READ_ERROR);
+	close_file_descriptor(destination_fd, WRITE_ERROR);
 }
 
 /**
@@ -109,8 +118,7 @@ int main(int argc, char *argv[])
 {
 	/* Check for correct number of arguments */
 	if (argc != 3)
-		print_error_and_exit("Usage: cp file_from file_to", 97);
-
+		print_error_and_exit("Usage: cp file_from file_to", USAGE_ERROR);
 	/* Copy content from source to destination file */
 	copy_file(argv[1], argv[2]);
 
