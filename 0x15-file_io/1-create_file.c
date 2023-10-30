3ae7870b2c6de095126e443a0429b9c9c592678f
Alogyn <mohamed.derfoufi.dev@gmail.com>
2023-10-30 15:09:40 -0700

    Create a file and writes text content to it

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,28 +16,31 @@
 int create_file(const char *filename, char *text_content)
 {
 	/* Syscall declarations */
-	int file_open = open(filename, O_WRONLY | O_CREAT |
-			O_TRUNC, S_IRUSR | S_IWUSR);
-	ssize_t file_write = write(file_open,
-			text_content, strlen(text_content));
+	int file_open, file_write;
 
 	/* In case 'filename' is empty */
 	if (!filename)
 		return (-1);
 
 	/* In case the file cannot be opned */
+	file_open = open(filename, O_WRONLY | O_CREAT |
+			O_TRUNC, S_IRUSR | S_IWUSR);
 	if (file_open == -1)
 		return (-1);
 
-	/* In case 'text_content' is not empty */
-	if (text_content)
+	/* In case 'text_content' is empty */
+	if (!text_content)
 	{
-		/* In case write action fail */
-		if (file_write == -1)
-		{
-			close(file_open);
-			return (-1);
-		}
+		close(file_open);
+		return (1);
+	}
+
+	file_write = write(file_open, text_content, strlen(text_content));
+	/* Checks the write action fail */
+	if (file_write < 0)
+	{
+		close(file_open);
+		return (-1);
 	}
 
 	close(file_open);
