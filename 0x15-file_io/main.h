3d751687df13a1aa4bf223246a8d7fcc84090448
Alogyn <mohamed.derfoufi.dev@gmail.com>
2023-10-30 15:11:22 -0700

    Prototypes functions

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -12,5 +12,7 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 /* Task 1 */
 int create_file(const char *filename, char *text_content);
+/* Task 2 */
+int append_text_to_file(const char *filename, char *text_content);
 
 #endif /* MAIN_H */
