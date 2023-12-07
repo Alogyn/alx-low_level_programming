ed92fc9b9ff711f32fb1c6f5fce4585c398bba45
Alogyn <mohamed.derfoufi.dev@gmail.com>
2023-12-07 13:57:25 -0800

    Add a new node at the beginning of a dlistint_t list

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/* Task */
+
+/**
+  * add_dnodeint - adds a new node at the beginning of a dlistint_t list
+  *
+  * @head: pointer to the pointer of the head of the list
+  * @n: value to be set in the new node
+  *
+  * Return: The address of the new element, or NULL if it failed
+  */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node;
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->prev = NULL;
+	new_node->next = *head;
+
+	if (*head != NULL)
+		(*head)->prev = new_node;
+
+	*head = new_node;
+
+	return (new_node);
+}
--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -3,6 +3,7 @@
 
 /* Standard Libreries */
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
   * struct dlistint_s - doubly linked list
@@ -25,5 +26,7 @@ typedef struct dlistint_s
 size_t print_dlistint(const dlistint_t *h);
 /* Task 1 */
 size_t dlistint_len(const dlistint_t *h);
+/* Task 2 */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n);
 
 #endif /* LISTS_H */
