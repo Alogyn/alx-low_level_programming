fba7ed35ca64a6426691a898a1eebbc660c71a80
Alogyn <mohamed.derfoufi.dev@gmail.com>
2023-12-07 13:46:36 -0800

    Print all the elements of a dlistint_t list

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/* Task 1. List length */
+
+/**
+  * dlistint_len - returns the number of elements in a linked dlistint_t list
+  *
+  * @h: pointer to the head of the list
+  *
+  * Return: The number of elements
+  */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		count++;
+	}
+
+	return (count);
+}
