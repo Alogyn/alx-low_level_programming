71425fc11e82362eb43ef2d49eda5e3ac30f7d0a
Alogyn <mohamed.derfoufi.dev@gmail.com>
2023-12-21 13:40:25 -0800

    Create a hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,23 +11,23 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *hash_table;
+	hash_table_t *table;
+	hash_node_t **array;
+	unsigned long int i;
 
-	if (size == 0)
+	table = malloc(sizeof(hash_table_t));
+	if (table == NULL)
 		return (NULL);
 
-	hash_table = malloc(sizeof(hash_table_t));
-	if (hash_table == NULL)
+	array = malloc(sizeof(hash_node_t *) * size);
+	if (array == NULL)
 		return (NULL);
 
-	hash_table->array = malloc(sizeof(hash_node_t *) * size);
-	if (hash_table->array == NULL)
-	{
-		free(hash_table);
-		return (NULL);
-	}
+	for (i = 0; i < size; i++)
+		array[i] = NULL;
 
-	hash_table->size = size;
+	table->array = array;
+	table->size = size;
 
-	return (hash_table);
+	return (table);
 }
