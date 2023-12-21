bad63eedb126879c178f3d4fdc46a83ba5feda40
Alogyn <mohamed.derfoufi.dev@gmail.com>
2023-12-21 13:38:25 -0800

    Create a hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -23,7 +23,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table->array = malloc(sizeof(hash_node_t *) * size);
 	if (hash_table->array == NULL)
 	{
-		free(hash_table); // Free hash_table if array allocation fails
+		free(hash_table);
 		return (NULL);
 	}
 
