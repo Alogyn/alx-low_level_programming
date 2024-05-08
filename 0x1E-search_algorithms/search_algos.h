21614729dd3476b6540161d38b127a348be88490
Alogyn <mohamed.derfoufi.dev@gmail.com>
2024-05-08 15:00:30 -0700

    Search for for a value in a sorted array of integers using the Jump search algorithm

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -45,6 +45,7 @@ typedef struct skiplist_s
 int linear_search(int *array, size_t size, int value);
 /* Task 1 */
 int binary_search(int *array, size_t size, int value);
+/* Task 7 (Advanced) */
 int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
