35f464aa441bba1ab60192a905f14614b9ff372a
Alogyn <mohamed.derfoufi.dev@gmail.com>
2024-05-08 15:13:50 -0700

    Search for a value in a sorted array of integers using the Exponential search algorithm

---
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -0,0 +1,54 @@
+#include "search_algos.h"
+
+/**
+  * interpolation_search - Searches for a value in an array of
+  * integers using the Interpolation search algorithm
+  *
+  * @array: Input array
+  * @size: Size of the array
+  * @value: Value to search in
+  *
+  * Return: Index of the number
+  */
+
+int interpolation_search(int *array, size_t size, int value)
+{
+	size_t pos, low, high;
+	double f;
+
+	if (array == NULL)
+		return (-1);
+
+	low = 0;
+	high = size - 1;
+
+	while (size)
+	{
+		f = (double)(high - low) / (array[high] - array[low]) * (value - array[low]);
+		pos = (size_t)(low + f);
+		printf("Value checked array[%d]", (int)pos);
+
+		if (pos >= size)
+		{
+			printf(" is out of range\n");
+			break;
+		}
+		else
+		{
+			printf(" = [%d]\n", array[pos]);
+		}
+
+		if (array[pos] == value)
+			return ((int)pos);
+
+		if (array[pos] < value)
+			low = pos + 1;
+		else
+			high = pos - 1;
+
+		if (low == high)
+			break;
+	}
+
+	return (-1);
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -47,6 +47,7 @@ int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 /* Task 7 (Advanced) */
 int jump_search(int *array, size_t size, int value);
+/* Task 9 (Advanced) */
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
