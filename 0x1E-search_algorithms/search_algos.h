1219db5912cde53741101a43611a3eaed4c611b2
Alogyn <mohamed.derfoufi.dev@gmail.com>
2024-05-08 15:28:57 -0700

    Search for a value in a sorted array of integers

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -49,7 +49,9 @@ int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
 /* Task 9 (Advanced) */
 int interpolation_search(int *array, size_t size, int value);
+/* Task 10 (Advanced) */
 int exponential_search(int *array, size_t size, int value);
+/* Task 11 (Advanced) */
 int advanced_binary(int *array, size_t size, int value);
 listint_t *jump_list(listint_t *list, size_t size, int value);
 skiplist_t *linear_skip(skiplist_t *list, int value);
